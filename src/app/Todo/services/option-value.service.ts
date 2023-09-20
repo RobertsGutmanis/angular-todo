1421c923575f8a80e564d6617a7ff451bbf481f2
robertsgutmanis <sapins23@gmail.com>
2023-09-20 17:13:06 +0300

    optionvalueservice updated from functon to getter

--- src/app/Todo/services/option-value.service.ts
+++ src/app/Todo/services/option-value.service.ts
@@ -2,10 +2,9 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class OptionValueService {
-  optionValues: string[] = ["type1", "type2", "type3"];
-
-  getOptionValues(): string[] {
-    return this.optionValues
+  get getOptionValues(): string[] {
+    return this._optionValues;
   }
+  private _optionValues: string[] = ["type1", "type2", "type3"];
 
 }
