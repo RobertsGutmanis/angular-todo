e37343dbdcfc0596bd277bb1742d04e4fb214753
RobertsGutmanis <sapins23@gmail.com>
2023-09-20 21:52:42 +0300

    env update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  Authorization: 'TzAPTpVgBpPm1c5vz5nZ0skEGsjdDipQteTMbcJ3rmZt6elRD1QeUgAO',
+  ApiEndpoint: 'https://api.pexels.com/v1/search?query=$'
 };
